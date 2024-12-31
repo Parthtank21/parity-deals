fca7e86db2593856dd168224c9d474c54f46a555
Parth Tank <parthtank301@gmail.com>
2024-12-31 12:56:34 +0530

    FEAT: implemented caching system

--- src/server/db/users.ts
+++ src/server/db/users.ts
@@ -1,12 +1,39 @@
-import { db } from "@/drizzle/db";
-import { ProductTable, UserSubscriptionTable } from "@/drizzle/schema";
 import { eq } from "drizzle-orm";
+import { db } from "@/drizzle/db";
+import { CACHE_TAGS, revalidateDbCache } from "@/lib/cache";
+import { ProductTable, UserSubscriptionTable } from "@/drizzle/schema";
 
-export function deleteUser(clerkUserId: string) {
-  return db.batch([
+export async function deleteUser(clerkUserId: string) {
+  const [usersubscriptions, products] = await db.batch([
     db
       .delete(UserSubscriptionTable)
-      .where(eq(UserSubscriptionTable.clerkUserId, clerkUserId)),
-    db.delete(ProductTable).where(eq(ProductTable.clerkUserId, clerkUserId)),
+      .where(eq(UserSubscriptionTable.clerkUserId, clerkUserId))
+      .returning({
+        id: UserSubscriptionTable.id,
+      }),
+    db
+      .delete(ProductTable)
+      .where(eq(ProductTable.clerkUserId, clerkUserId))
+      .returning({
+        id: ProductTable.id,
+      }),
   ]);
+
+  usersubscriptions.forEach((sub) => {
+    revalidateDbCache({
+      tag: CACHE_TAGS.subscription,
+      userId: clerkUserId,
+      id: sub.id,
+    });
+  });
+
+  products.forEach((product) => {
+    revalidateDbCache({
+      tag: CACHE_TAGS.products,
+      userId: clerkUserId,
+      id: product.id,
+    });
+  });
+
+  return [usersubscriptions, products];
 }
