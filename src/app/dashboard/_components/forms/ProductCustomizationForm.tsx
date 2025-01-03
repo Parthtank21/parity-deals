ce73512479c0e155cf9245ff1eaf7481be444e28
Parth Tank <parthtank301@gmail.com>
2025-01-03 11:56:47 +0530

    FEAT: completed banner customization form UI

--- src/app/dashboard/_components/forms/ProductCustomizationForm.tsx
+++ src/app/dashboard/_components/forms/ProductCustomizationForm.tsx
@@ -1,4 +1,25 @@
+"use client";
+
+import { z } from "zod";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { productCustomizationSchema } from "@/schemas/product";
+
+import { Input } from "@/components/ui/input";
+import { Switch } from "@/components/ui/switch";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import RequiredLabelIcon from "@/components/RequiredLabelIcon";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 export default function ProductCustomizationForm({
   customization,
@@ -18,5 +39,148 @@ export default function ProductCustomizationForm({
   canRemoveBranding: boolean;
   canCustomizeBanner: boolean;
 }) {
-  return <div>ProductCustomizationForm</div>;
+  const form = useForm<z.infer<typeof productCustomizationSchema>>({
+    resolver: zodResolver(productCustomizationSchema),
+    defaultValues: {
+      ...customization,
+      classPrefix: customization.classPrefix ?? "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof productCustomizationSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form
+          className="flex gap-6 flex-col mt-8"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <div className="grid gap-8 grid-cols-1 md:grid-cols-2">
+            <FormField
+              control={form.control}
+              name="locationMessage"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    PPP Discount Message
+                    <RequiredLabelIcon />
+                  </FormLabel>
+                  <FormControl>
+                    <Textarea
+                      disabled={!canCustomizeBanner}
+                      className="min-h-20 resize-none"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    {"Data Parameters: {country}, {coupon}, {discount}"}
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <FormField
+                control={form.control}
+                name="backgroundColor"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Background Color
+                      <RequiredLabelIcon />
+                    </FormLabel>
+                    <FormControl>
+                      <Input disabled={!canCustomizeBanner} {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="fontSize"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Font Size
+                      <RequiredLabelIcon />
+                    </FormLabel>
+                    <FormControl>
+                      <Input disabled={!canCustomizeBanner} {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="bannerContainer"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Banner Container
+                      <RequiredLabelIcon />
+                    </FormLabel>
+                    <FormControl>
+                      <Input disabled={!canCustomizeBanner} {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      HTML container selector where you want to place the
+                      banner. Ex: #container, .container, body
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="classPrefix"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>CSS Prefix</FormLabel>
+                    <FormControl>
+                      <Input disabled={!canCustomizeBanner} {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      An optional prefix added to all CSS classes to avoid
+                      conflicts
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="isSticky"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Sticky?</FormLabel>
+                    <FormControl>
+                      <Switch
+                        className="block"
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                        disabled={!canCustomizeBanner}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+          {canCustomizeBanner && (
+            <div className="self-end">
+              <Button disabled={form.formState.isSubmitting} type="submit">
+                Save
+              </Button>
+            </div>
+          )}
+        </form>
+      </Form>
+    </>
+  );
 }
